03842db463886168cc668eac9f60e744ddd42679
Jonathann8 <jtvieira30@gmail.com>
2024-08-15 19:02:30 -0300

    Create Exemplo de função.py

---
+++ First Sparks/Exemplo de função.py
@@ -0,0 +1,8 @@
+def soma(a, b):
+    resultado = a + b
+    return resultado
+
+# Chamando a função e armazenando o retorno
+resultado_soma = soma(5, 7)
+print("O resultado da soma é:", resultado_soma)
+
