eb7a93e99452e86f9c70b305b66f6f57db04268e
Jonathan Vieira <jtvieira30@gmail.com>
2025-07-26 18:20:55 -0300

    Update app.py

--- Teste API/app.py
+++ Teste API/app.py
@@ -1,23 +1,29 @@
 from flask import Flask, jsonify, request
 
-app = flask(__name__)
+app = Flask(__name__)
 
 livros = [
     {
         'id': 1,
         'titulo': 'A Metamorfose',
         'autor': 'Franz Kafka',
-    }
+    },
     {
         'id': 2,
         'titulo': 'O Arqueiro',
-        'autor': ' Bernard Cornwell',
+        'autor': 'Bernard Cornwell',
     },
     {       
-         'id': 3,
+        'id': 3,
         'titulo': 'O Senhor dos Anéis',
         'autor': 'J.R.R. Tolkien',
     },
 ]
 
- 
+@app.route('/livros')
+def obter_livros():
+    return jsonify(livros)
+
+# Executar a aplicação
+if __name__ == '__main__':
+    app.run(port=5000, host='localhost', debug=True)
