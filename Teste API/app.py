d8663b5b6fd442516d1e0ae45a1207f3213039cd
Jonathan Vieira <jtvieira30@gmail.com>
2025-07-26 18:13:55 -0300

    Update app.py

--- Teste API/app.py
+++ Teste API/app.py
@@ -2,4 +2,22 @@ from flask import Flask, jsonify, request
 
 app = flask(__name__)
 
+livros = [
+    {
+        'id': 1,
+        'titulo': 'A Metamorfose',
+        'autor': 'Franz Kafka',
+    }
+    {
+        'id': 2,
+        'titulo': 'O Arqueiro',
+        'autor': ' Bernard Cornwell',
+    },
+    {       
+         'id': 3,
+        'titulo': 'O Senhor dos Anéis',
+        'autor': 'J.R.R. Tolkien',
+    },
+]
+
  
